c4434256077d6cf6204bf0f38325184e1745e5a6
Hebert de Oliveira Ferreira <hoferreira@ecad.org.br>
2020-03-09 11:01:52 -0300

    Criação da CategoriaMapping

---
+++ SGC/src/SGC.Data/Mapeamentos/CategoriaMapping.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SGC.Business.Models.Entidades;
+
+namespace SGC.Data.Mapeamentos
+{
+    public class CategoriaMapping : IEntityTypeConfiguration<Categoria>
+    {
+        #region Public Methods
+
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.NomeCategoria)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.DescricaoCategoria)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.OperadorId)
+                .IsRequired();
+
+            builder.ToTable("Categorias");
+        }
+
+        #endregion Public Methods
+    }
+}
