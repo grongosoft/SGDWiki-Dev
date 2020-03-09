ff59321fd5ae39754ba37c678f74f463d1fd916c
Hebert de Oliveira Ferreira <hoferreira@ecad.org.br>
2020-03-09 10:39:11 -0300

    Inserido Id de Usuario na Categoria

--- SGC/src/SGC.Business/Models/Entidades/Categoria.cs
+++ SGC/src/SGC.Business/Models/Entidades/Categoria.cs
@@ -4,6 +4,7 @@
     //TODO: CLASSE BASE ADICIONAR OPERADOR!
     public class Categoria : Entity
     {
+        public string OperadorId { get; set; }
         public string DescricaoCategoria { get; set; }
         public string NomeCategoria { get; set; }
 
