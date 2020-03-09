d043cd542ca4c79437db3b09abbfb53f82284b1f
Hebert de Oliveira Ferreira <hoferreira@ecad.org.br>
2020-03-09 11:39:12 -0300

    Criado uma classe para Injeção de Dependências dos serviços que o Startup usa.

---
+++ SGC/src/SGD.App/Configuracoes/IdentityConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SGD.App.Data;
+
+namespace SGD.App.Configuracoes
+{
+    public static class IdentityConfig
+    {
+        #region Public Methods
+
+        public static IServiceCollection AdicionarIdentityDeConfiguracao(this IServiceCollection servicos, IConfiguration configuracoes)
+        {
+            servicos.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = contexto => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            servicos.AddDbContext<ApplicationDBContext>(options =>
+                options.UseSqlServer(configuracoes.GetConnectionString("DefaultConnection")));
+
+            return servicos;
+        }
+
+        #endregion Public Methods
+    }
+}
--- SGC/src/SGD.App/Startup.cs
+++ SGC/src/SGD.App/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SGD.App.Configuracoes;
 
 namespace SGD.App
 {
@@ -31,21 +32,19 @@ namespace SGD.App
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+   
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AdicionarIdentityDeConfiguracao(Configuration);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -55,7 +54,6 @@ namespace SGD.App
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
