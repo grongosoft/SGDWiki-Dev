1660f2af55e89c14b2f157eec51ff7bb46bbbf36
Hebert de Oliveira Ferreira <hoferreira@ecad.org.br>
2020-04-06 11:32:22 -0300

    Adicionado Campo categoria fazendo a pesquisa e preenchendo corretamente na tela de pesquisa

--- SGC/src/SGD.App/Controllers/PerguntaRespostasController.cs
+++ SGC/src/SGD.App/Controllers/PerguntaRespostasController.cs
@@ -3,14 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SGC.Business.Interfaces;
+using SGC.Data.Contexto;
+using SGD.App.ViewModel;
 
 namespace SGD.App.Controllers
 {
     public class PerguntaRespostasController : Controller
     {
+        private readonly DataContext _context;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public PerguntaRespostasController(DataContext context, ICategoriaRepository categoriaRepository)
+        {
+            _context = context;
+            _categoriaRepository = categoriaRepository;
+
+        }
+
+        [Route("filtro-pergunta_resposta")]
         public IActionResult Index()
         {
-            return View();
+            var perguntaRepostaViewModel = BuscarCategoriasCadastradas();
+
+            if (perguntaRepostaViewModel.Result.CategoriaViewModel == null)
+                return RedirectToAction("Index");
+
+            return View(perguntaRepostaViewModel.Result);
+        }
+
+
+        private async Task<PerguntaRespostaViewModel> BuscarCategoriasCadastradas()
+        {
+            var categoriaUsuario = new List<CategoriaViewModel>();
+            var categorias = await _categoriaRepository.Listar();
+
+            var perguntaRepostaViewModel = new PerguntaRespostaViewModel
+            {
+                CategoriaViewModel = new List<CategoriaViewModel>()
+            };
+
+
+
+            foreach (var categoria in categorias)
+            {
+                var categoriaViewModel = new CategoriaViewModel
+                {
+                    Nome = categoria.Nome
+                };
+
+
+
+                perguntaRepostaViewModel.CategoriaViewModel.Add(categoriaViewModel);
+            }
+
+
+            return perguntaRepostaViewModel;
+
         }
     }
 }
--- SGC/src/SGD.App/ViewModel/PerguntaRespostaViewModel.cs
+++ SGC/src/SGD.App/ViewModel/PerguntaRespostaViewModel.cs
@@ -1,9 +1,12 @@
-﻿using SGC.Common.Enum;
+﻿using System.Collections.Generic;
+using SGC.Common.Enum;
 
 namespace SGD.App.ViewModel
 {
     public class PerguntaRespostaViewModel
     {
         public ELike Like { get; set; }
+
+        public List<CategoriaViewModel> CategoriaViewModel { get; set; }
     }
 }
